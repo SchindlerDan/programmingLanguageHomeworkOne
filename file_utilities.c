d11d519b472c698cc477c50dcc5e74da8869ee8b
Daniel Schindler <schindld@dc18.cis.gvsu.edu>
2018-01-26 17:12:07 -0500

    began working on the file utilities code. About to try a test on reading files

--- file_utilities.c
+++ file_utilities.c
@@ -1,9 +1,36 @@
 int read_file( char* filename, char** buffer){
+	//used some code from CIS 241
+	File *in;
 
+	in = fopen("test.txt", "r");
+	if(in == NULL){
+		printf("This file could not be found");
+		exit(1);
+	}
+	int columns;
+	int rows;
+	fscanf(in, %d, &columns);
+	fscanf(in, %d, &rows);
+	char[rows][columns] map;
+	int x;
+	int y;
+	//aasked Brendon Murthum and Tanner Gibson about fscanf
+	//went to tutorialspoint.com to confirm that C for loops are created like java and C++ for loops
+	for(x = 0; x < rows; x++ ){
+		for(y = 0; y < colums; y++){
+			fscanf(in, %c, map[x][y]);
+		}
+	}
 
+	free(*buffer);
+	*buffer = &map;
 
-
-
+	for(x = 0; x < rows; x++){
+		for(y = 0; y < columns; y++){
+			fprintf(%c, map[x][y]);
+		}
+	
+	}
 
 }
 
