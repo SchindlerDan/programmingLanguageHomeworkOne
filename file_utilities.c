f3c6ac6e5a5c3f337c1453bccae60d64f56fcb7c
Daniel Schindler <schindld@dc18.cis.gvsu.edu>
2018-01-26 18:03:35 -0500

    Further work on file_utilities

--- file_utilities.c
+++ file_utilities.c
@@ -1,33 +1,38 @@
+//I forgot that we needed to include the .h file -Dan
+#include "file_utilities.h"
+
+
 int read_file( char* filename, char** buffer){
 	//used some code from CIS 241
-	File *in;
+	FILE *in;
 
-	in = fopen("test.txt", "r");
+	in = fopen("blank1010.txt", "r");
 	if(in == NULL){
 		printf("This file could not be found");
 		exit(1);
 	}
 	int columns;
 	int rows;
-	fscanf(in, %d, &columns);
-	fscanf(in, %d, &rows);
-	char[rows][columns] map;
+	fscanf(in, "%d", &columns);
+	fscanf(in, "%d", &rows);
+	//char* map = malloc(rows * columns * sizeof(char));
+	char map[rows][columns];
 	int x;
 	int y;
 	//aasked Brendon Murthum and Tanner Gibson about fscanf
 	//went to tutorialspoint.com to confirm that C for loops are created like java and C++ for loops
 	for(x = 0; x < rows; x++ ){
-		for(y = 0; y < colums; y++){
-			fscanf(in, %c, map[x][y]);
+		for(y = 0; y < columns; y++){
+			fscanf(in, "%c", map[x][y]);
 		}
 	}
 
 	free(*buffer);
-	*buffer = &map;
+	*buffer = map;
 
 	for(x = 0; x < rows; x++){
 		for(y = 0; y < columns; y++){
-			fprintf(%c, map[x][y]);
+			fprintf("%c", map[x][y]);
 		}
 	
 	}
