e8ebc6a7a3a8a574f3675b55fdfc5832c19b672a
emymorningstar <emymorningstar@gmail.com>
2018-01-28 23:22:52 -0500

    added running loop and quitability

--- file_utilities.c
+++ file_utilities.c
@@ -2,64 +2,72 @@
 #include "file_utilities.h"
 
 
-int read_file( char* filename, char** buffer){
-	//used some code from CIS 241
-	FILE *in;
+int read_file( char* filename, char** buffer)
+{
+    //used some code from CIS 241
+    FILE *in;
+//TODO add a input to take in a string
 
-	in = fopen("blank1010.txt", "r");
-	if(in == NULL){
-		printf("This file could not be found");
-		exit(1);
-	}
-	int columns;
-	int rows;
-	fscanf(in,"%d", &columns);
-	fscanf(in, "%d", &rows);
+    in = fopen(filename, "r");
+    if(in == NULL)
+    {
+        printf("This file could not be found");
+        exit(1);
+    }
+    int columns;
+    int rows;
+    fscanf(in,"%d", &columns);
+    fscanf(in, "%d", &rows);
 
-	//https://stackoverflow.com/questions/10575544/difference-between-array-type-and-array-allocated-with-malloc
-	//used Stack Overflow to decide how to create the 2d arrayi
-	char* map = malloc(rows * columns * sizeof(char));
+    //https://stackoverflow.com/questions/10575544/difference-between-array-type-and-array-allocated-with-malloc
+    //used Stack Overflow to decide how to create the 2d arrayi
+    char* map = malloc(rows * columns * sizeof(char));
 
-	//char map[rows][columns];
-	int x;
-	int y;
-	//aasked Brendon Murthum and Tanner Gibson about fscanf
-	//went to tutorialspoint.com to confirm that C for loops are created like java and C++ for loops
-	for(x = 0; x < rows; x++ ){
-		for(y = 0; y < columns; y++){
-			fscanf(in, "%c", map[(x * rows) + y]);
-		}
-	}
+    //char map[rows][columns];
+    int x;
+    int y;
+    //aasked Brendon Murthum and Tanner Gibson about fscanf
+    //went to tutorialspoint.com to confirm that C for loops are created like java and C++ for loops
+    for(x = 0; x < rows; x++ )
+    {
+        for(y = 0; y < columns; y++)
+        {
+            fscanf(in, "%c", map[(x * rows) + y]);
+        }
+    }
 
-	free(*buffer);
-	*buffer = map;
+    free(*buffer);
+    *buffer = map;
 
-	for(x = 0; x < rows; x++){
-		for(y = 0; y < columns; y++){
-			printf("%c", map[(x * rows) + y]);
-		}
-	
-	}
+    for(x = 0; x < rows; x++)
+    {
+        for(y = 0; y < columns; y++)
+        {
+            printf("%c", map[(x * rows) + y]);
+        }
 
+    }
 }
 
 
 
-int write_file( char* filename, char *buffer, int size){
-	FILE *out;
-	//Once again used some code from a CIS 241 assignment
-	out =fopen(filename, "w");
+int write_file( char* filename, char *buffer, int size)
+{
+    FILE *out;
+    //Once again used some code from a CIS 241 assignment
+    out =fopen(filename, "w");
 
-	if(out == NULL){
-	printf("File could not be found or created");
-	}
-	
-	int i = 0;
+    if(out == NULL)
+    {
+        printf("File could not be found or created");
+    }
 
-	for(i = 0; i < size; i++){
-		fprintf(out, "%c", buffer[i]);
-		fprintf(out, " ");
-	}
+    int i = 0;
 
+    for(i = 0; i < size; i++)
+    {
+        fprintf(out, "%c", buffer[i]);
+        fprintf(out, " ");
+    }
 
 }
--- main.c
+++ main.c
@@ -1,19 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "file_utilities.h"
+
+int main(int argc, const char** argv)
+{
+
+    int numRow = argv[1];
+    int numCol = argv[2];
 
 
-int main(int argc, const char** argv){
 
-	int numRow = argv[1];
-	int numCol = argv[2];
-	
-	
-	
-	//used code from an assignment from CIS 241
-	FILE *in, *out;
-	char ch; 
+    //used code from an assignment from CIS 241
+    FILE *in, *out;
+    char ch;
+//to see if it starts on a load
+    if(argc>3||argc==2)
+    {
+        //I wrote this  -emy
+      const char* filename= argv[3];
+        char** buffer;
+        read_file(filename,buffer);
+    }
+    else
+    {
+        //create a blank file of specifed size
+    }
+    //game loop
+    int quit=0;
+    while(quit==0)
+    {
+        char choice;
+        int times=0;
+        //check for next input
+        printf("q to quit, w to write, l to load, r to run once, m to run many");
+        //take input entered
+        //stack overflow to figure out how to ignore extra white space
+        //https://stackoverflow.com/questions/20712572/how-to-ignore-whitespaces-in-fscanf
+        scanf(" %c",&choice);
+        //do the selection
+       if(choice=='q')
+        {
+            printf("bye");
+            quit=1;
+        }
+        else{
+            printf("hi");
+        }
 
 
+        for(times;times>0;times--)
+        {
+            //for loop for checking death rates and breeding rates
+        }
+
+    }
 
 
 }
